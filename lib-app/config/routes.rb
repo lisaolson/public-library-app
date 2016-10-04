3c7a574fea01e77fddf3afd58427345fa8ad8122
Lisa Olson <olson.lisa94@gmail.com>
2016-10-04 16:15:39 -0700

    Adds routes for new user and begins login

--- lib-app/config/routes.rb
+++ lib-app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
+  get '/login', to: 'sessions#new'
 
 end
