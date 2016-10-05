a33b960af9089f3daa13c00a719c090f9315406e
Lisa Olson <olson.lisa94@gmail.com>
2016-10-05 10:14:01 -0700

    Adds get and post route for new libraray w/ controller & views

--- lib-app/config/routes.rb
+++ lib-app/config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
+  get '/libraries', to: 'libraries#index'
+  get '/libraries/new', to: 'libraries#new', as: 'new_library'
+  post '/libraries', to: 'libraries#create'
+
 end
